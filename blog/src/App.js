97084c7441aa58194875295ece9c708fff4739f4
shaheer21-sys <65283693+shaheer21-sys@users.noreply.github.com>
2023-07-15 20:11:39 +0530

    removed basic and added react blog app

---
+++ blog/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Container from './component/Container';
+import Footer from './component/Footer';
+import Header from './component/Header';
+import Navbar from './component/Navbar';
+
+function App() {
+  return (
+    <div >
+   
+        <Navbar/>
+      
+        <Header/>
+       
+        <Container/>
+
+        <Footer/>
+    </div>
+  );
+}
+
+export default App;
